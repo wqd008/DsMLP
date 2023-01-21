10565b89d03f209c095ec1b08048f66f6c0364cc
wqd008 <45097208+wqd008@users.noreply.github.com>
2023-01-21 09:38:15 +0800

    Delete Ex1_DsNet_Twin_and_MMSE_and_Richardson directory

--- Ex1_DsNet_Twin_and_MMSE_and_Richardson/and_door.m
+++
@@ -1,7 +0,0 @@
-function output = and_door(x,y)
-if (x==1&&y==1)
-    output=1;
-else 
-    output=0;
-end
-end
--- Ex1_DsNet_Twin_and_MMSE_and_Richardson/xor_door.m
+++
@@ -1,7 +0,0 @@
-function output = xor_door(x,y)
-if (x==1&&y==0)||(x==0&&y==1)
-    output=1;
-else 
-    output=0;
-end
-end
